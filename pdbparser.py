654954d63e863856ff5b5172f16795c9ab88cf8f
Kevin <hbyang2000@gmail.com>
2022-04-23 01:24:24 -0400

    Added gitignore + docstrings to parser and residue structure

--- pdbparser.py
+++ pdbparser.py
@@ -1,26 +1,33 @@
 import pickle
-import math
 from pathlib import Path
 import numpy as np
 from structures import *
-from utils import *
 
 class PDBParser:
 	'''
 	Parser for .pdb files into a Complex structure
 	'''
-	def __init__(self,verbose=False):
+	def __init__(self,verbose=False,local=True):
 		'''
-		Parses protein data
-		into a ProteinStructure
+		Initializes PDBParser object
+		Call parse_pdb to parse a certain file
 
 		Parameters:
 			verbose : bool :: Debug output toggle during parsing
+			local : bool :: Calculate local system for residues
 
 		'''
 		self.verbose = verbose
+		self.local = local
 
 	def parse_pdb(self, path_to_pdb):
+		'''
+		Opens .pdb file and
+		parses lines into Complex structure
+
+		Parameters:
+			path_to_pdb : string :: path to a .pdb file
+		'''
 		path = Path(path_to_pdb)
 		assert path.suffix == '.pdb', 'PDB file needs to end in .pdb'
 		
@@ -31,14 +38,15 @@ class PDBParser:
 		complex_name = complex_file_name.split('_')[0]
 		complex_ligand_id = complex_file_name.split('_')[1]
 		complex_protein_id = complex_file_name.split('_')[2]
-		complex_structure = Complex(complex_name, proteinID=complex_protein_id, ligandID=complex_ligand_id)
+		complex_structure = Complex(complex_name, proteinID=complex_protein_id,
+									ligandID=complex_ligand_id)
 		
 		current_chainID = None
 		current_resSeq = None
 		current_residue_atom_list = []
-		protein_residue = Residue('init','residue',-1)
+		residue = Residue('init','residue')
 
-		with path.open() as file:
+		with path.open(encoding="utf-8") as file:
 			for line in file:
 				line = line.rstrip()
 				parsed_pdb_row = parse_pdb_row(line)
@@ -46,14 +54,14 @@ class PDBParser:
 				if current_chainID != parsed_pdb_row['chainID']:
 					current_chainID = parsed_pdb_row['chainID']
 					chain_structure = Chain(parsed_pdb_row['chainID'])
-					complex_structure.add_structure(chain_structure)
+					complex_structure.add_chain(chain_structure)
 
 				if current_resSeq != parsed_pdb_row['resSeq']:
 					current_resSeq = parsed_pdb_row['resSeq']
-					protein_residue.add_atoms(current_residue_atom_list)
+					residue.add_atoms(current_residue_atom_list)
 					current_residue_atom_list = []
-					protein_residue = ProteinResidue(parsed_pdb_row['name'],
-														int(parsed_pdb_row['resSeq']))
+					residue = Residue(parsed_pdb_row['name'],
+										int(parsed_pdb_row['resSeq']),self.local)
 					chain_structure.add_residue(residue)
 
 				coordinate = np.array([
@@ -64,13 +72,17 @@ class PDBParser:
 				
 				atom = Atom(parsed_pdb_row['name'],parsed_pdb_row['element'], coordinate,
 									parsed_pdb_row['altLoc'] == 'CA')
-				if parsed_pdb_row['altLoc'] == 'CA':
-					protein_residue.add_alpha_carbon(atom)
-				else:
-					current_residue_atom_list.append(atom)
+				current_residue_atom_list.append(atom)
 		return complex_structure
 
 	def parse_pdb_to_pickle(self,path_to_pdb, pickle_path=None):
+		'''
+		Parses .pdb into a pickle that stores Complex structure
+
+		Parameters:
+			path_to_pdb : string :: Path to .pdb file
+			pickle_path : string :: Optional path to store pickle
+		'''
 		if pickle_path is None:
 			path = Path(path_to_pdb)
 			complex_file_name = path.stem
@@ -80,14 +92,27 @@ class PDBParser:
 		with path.open() as f:
 			pickle.dump(complex_structure, f)
 
-	def parse_pdb_from_pickle(self,pickle_path):
-		path = Path(pickle_path)
-		with path.open() as f:
-			complex_structure = pickle.dump(complex_structure)
-		return complex_structure
+def parse_pdb_from_pickle(pickle_path):
+	'''
+	Gets Complex structure from pickle file
+
+	Parameters:
+		pickle_path : string :: Path to pickle file
+	'''
+	path = Path(pickle_path)
+	with path.open() as f:
+		complex_structure = pickle.dump(f)
+	return complex_structure
 
 
 def parse_pdb_row(pdb_row):
+	'''
+	Parses a row of a .pdb file
+	Specifically ATOM/HETATOM row
+
+	Parameters:
+		pdb_row : string :: Row of .pdb file
+	'''
 	indices = {'ATOM':(0,3),
 			'HETATOM':(0,5),
 			'serial':(6,10),
@@ -100,10 +125,20 @@ def parse_pdb_row(pdb_row):
 			'y':(38,45),
 			'z':(46,53),
 			'element':(76,77)}
+	row_length = len(pdb_row)
+	if len(pdb_row) < 78:
+		raise PDBError(f"Row has {row_length} characters but needs atleast 78")
 	parsed_pdb_row = {}
 	for item, indice_tup in indices.items():
 		parsed_pdb_row[item] = pdb_row[indice_tup[0]:indice_tup[1]+1]
 	return parsed_pdb_row
 
 class FileNameError(Exception):
-	pass
+	''' 
+	Exception thrown when filename incorrect
+	'''
+
+class PDBError(Exception):
+	'''
+	Exception thrown when there is a .pdb file related error
+	'''
--- structures/residue.py
+++ structures/residue.py
@@ -1,5 +1,11 @@
+import math
+import numpy as np
+
 class Residue:
-	def __init__(self,name, resSeq):
+	'''
+	Structure that holds the represenation of a Residue
+	'''
+	def __init__(self,name, resSeq, local=True):
 		'''
 		Abstraction of a residue in a protein
 		A residue is defined by the name, chain ID, sequence number
@@ -8,11 +14,12 @@ class Residue:
 			name : str :: name of the residue
 			chainID : str :: chain identifier
 			resSeq : int :: residue sequence number
+			local : bool :: boolean if local system needs to be calculated
 		'''
 		self.name = name
 		self.resSeq = resSeq
-		self.alpha_carbon = alpha_carbon
-		self.coordinate = alpha_carbon.coordinate
+		self.alpha_carbon = None
+		self.coordinate = None
 		# carboxyl group (carbon, oxygen)
 		self.carboxyl = [None,None]
 		# amino group (nitrogen, hydrogen)
@@ -24,22 +31,54 @@ class Residue:
 		self.n = None
 		self.atoms = []
 
+		self.local = local
+
 	def add_alpha_carbon(self, carbon=None):
+		'''
+		Sets the alpha carbon of the residue
+		Alpha carbon acts as the coordinate of the residue
+
+		Parameters:
+			carbon : Atom :: Carbon atom that represents the alpha carbon
+		'''
+		if carbon.name != 'CA':
+			raise ResidueError('Alpha Carbon must have name CA')
 		self.alpha_carbon = carbon
 
 	def add_carboxyl(self, carbon=None, oxygen=None):
+		'''
+		Sets the carboxyl group of the residue
+
+		Parameters:
+			carbon : Atom :: Carbon atom of the carboxyl
+			oxygen : Atom :: Oxygen atom of the carboxyl
+		'''
+		if carbon.name != 'C':
+			raise ResidueError('Carboxyl Carbon must have name C')
+		if oxygen.name != 'O':
+			raise ResidueError('Carboxyl Oxygen must have name O')
 		if carbon is not None:
 			self.carboxyl[0] = carbon
 		if oxygen is not None:
 			self.carboxyl[1] = oxygen
 
 	def add_amino(self, nitrogen=None, hydrogen=None):
+		'''
+		Sets the amino group of the residue
+
+		Parameters:
+			nitrogen : Atom :: Nitrogen atom of the amino
+			oxygen : Atom :: Oxygen atom of the amino
+		'''
 		if nitrogen is not None:
 			self.amino[0] = nitrogen
 		if hydrogen is not None:
 			self.amino[1] = hydrogen
 
-	def calculate_local_system(self):
+	def check_valid_residue(self):
+		'''
+		Asserts that the residue has a valid configuration
+		'''
 		if self.alpha_carbon is None:
 			raise ResidueError('Missing Alpha Carbon')
 		if self.carboxyl[0] is None:
@@ -50,34 +89,63 @@ class Residue:
 			raise ResidueError('Missing Amino Nitrogen')
 		if self.amino[1] is None:
 			raise ResidueError('Missing Amino Oxygen')
-		self.u = carbon.set_vec(self.alpha_carbon)
-		self.t = nitrogen.set_vec(self.alpha_carbon)
-		u_cross_t = np.cross(self,u,self.t)
+
+	def calculate_system(self):
+		'''
+		Calculates the local structure system of the residue
+		Need to call this after all atoms are added
+		'''
+		self.check_valid_residue()
+		self.u = self.carboxyl[0].set_vec(self.alpha_carbon)
+		self.t = self.amino[0].set_vec(self.alpha_carbon)
+		u_cross_t = np.cross(self,self.u,self.t)
 		self.n = u_cross_t/np.linalg.norm(u_cross_t)
 		self.v = np.cross(self.n,self.u)
 
 	def get_coordinate(self):
+		'''
+		Returns the coordinate of the residue
+		Equivalent to the coordinates of the alpha carbon
+		'''
+		if self.alpha_carbon is None:
+			raise ResidueError('Missing Alpha Carbon')
 		return self.coordinate
 
 	def get_system(self):
+		'''
+		Returns the local structure system (n,u,v)
+		'''
 		return self.n, self.u, self.v 
 
-	def get_id(self):
-		return self.chainID
-
 	def get_segid(self):
+		'''
+		Returns the sequence number the residue is
+		'''
 		return self.resSeq
 
 	def get_group(self, name):
+		'''
+		Returns the group with the name parameter
+		Will be a single atom if alpha_carbon, else a tuple
+		returns None name is not valid
+
+		Parameters:
+			name : string :: Name of the group 
+		'''
 		if name == 'alpha':
 			return self.alpha_carbon
 		if name == 'carboxyl':
 			return self.carboxyl
-		elif name == 'amino':
+		if name == 'amino':
 			return self.amino
 		return None
 
 	def get_atoms(self):
+		'''
+		Generator that returns all of the atoms of the residue
+		In the order of alpha carbon, carboxyl atoms, amino atoms et al
+		'''
+		self.check_valid_residue()
 		yield self.alpha_carbon
 		for atom in self.carboxyl:
 			yield atom
@@ -87,6 +155,11 @@ class Residue:
 			yield atom
 
 	def add_atoms(self, atoms):
+		'''
+		Adds list of atoms to the residue
+		Will automatically calculate local system if needed
+		Returns number of atoms that are not C, O, N, H and CA
+		'''
 		unknown_atom_count = 0
 		for atom in atoms:
 			if atom.name == 'C':
@@ -105,24 +178,47 @@ class Residue:
 				self.add_alpha_carbon(carbon=atom)
 			self.atoms.append(atom)
 			unknown_atom_count += 1
+		if self.local:
+			self.calculate_system()
 		return unknown_atom_count == 0
 
 	def get_relative_position_edge_features(self, coordinate):
+		'''
+		Calculates the relative position edge features of the residue
+
+		Parameters:
+			coordinate : numpy array :: coordinate of an residue
+		'''
 		system_tup = (self.n.T, self.u.T, self.v.t)
 		return np.vstack(system_tup)@(coordinate - self.coordinate)
 
 	def get_relative_orientation_edge_features(self,system):
+		'''
+		Calculates the relative orientation edge features of the residue
+
+		Parameters:
+			system : tuple :: tuple of system vectors
+		'''
 		n = system[0]
 		u = system[1]
 		v = system[2]
 		system_tup = (n,u,v)
-		return np.vstack(system_tup)@n, np.vstack(system_tup)@u, np.vstack(system_tup)@v
+		return np.vstack(system_tup)@self.n, np.vstack(system_tup)@self.u, np.vstack(system_tup)@self.v
 
 	def get_distance_edge_features(self, coordinate, sigmas):
+		'''
+		Calculates the distance edge features of the residue
+
+		Parameters:
+			coordinate : numpy array :: coordinate of an residue
+			sigmas : list like :: list of sigmas for calculations
+		'''
 		f = []
 		for sigma in sigmas:
 			f.append(math.exp(np.linalg.norm(coordinate - self.coordinate)**2/(2*sigma**2)))
 		return f
 
 class ResidueError(Exception):
-	pass
+	'''
+	Exception for all Residue errors
+	'''
