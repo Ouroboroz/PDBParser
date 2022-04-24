4f914ba79be1f9ff12b9f861e5797b61f6e6475d
Kevin <hbyang2000@gmail.com>
2022-04-24 19:59:59 -0400

    Added verbosity to main parser

--- pdbparser.py
+++ pdbparser.py
@@ -14,9 +14,9 @@ class PDBParser:
 		Call parse_pdb to parse a certain file
 
 		Parameters:
-			verbose : bool, default - False :: Debug output toggle during parsing
-			local : bool, default - True :: Calculate local system for residues
-
+			verbose : int, default - False :: Debug output toggle during parsing
+						value controls the amount of debug statement
+			local : bool, default - True :: Calculate local system for residue
 		'''
 		self.verbose = verbose
 		self.local = local
@@ -30,7 +30,8 @@ class PDBParser:
 			path_to_pdb : string :: path to a .pdb file
 		'''
 		path = Path(path_to_pdb)
-		assert path.suffix == '.pdb', 'PDB file needs to end in .pdb'
+		if path.suffix !='.pdb':
+			raise FileNameError('PDB file needs to end in .pdb')
 		
 		complex_file_name = path.stem
 		#complex file name should be in format (complex id)_(chainID)_(chainID)
@@ -41,7 +42,9 @@ class PDBParser:
 		complex_protein_id = complex_file_name.split('_')[2]
 		complex_structure = Complex(complex_name, proteinID=complex_protein_id,
 									ligandID=complex_ligand_id)
-		
+		if self.verbose:
+			print(f"Parsing {complex_name} with chains {complex_protein_id} and {complex_ligand_id}")
+
 		current_chainID = None
 		current_resSeq = None
 		current_residue_atom_list = []
@@ -50,14 +53,21 @@ class PDBParser:
 		with path.open(encoding="utf-8") as file:
 			for line in file:
 				line = line.rstrip()
+				if self.verbose > 1:
+					print('  Parsing', line)
+
 				parsed_pdb_row = parse_pdb_row(line)
 
 				if current_chainID != parsed_pdb_row['chainID']:
+					if self.verbose > 2:
+						print(f"    Starting Chain: {parsed_pdb_row['chainID']}")
 					current_chainID = parsed_pdb_row['chainID']
 					chain_structure = Chain(parsed_pdb_row['chainID'])
 					complex_structure.add_chain(chain_structure)
 
 				if current_resSeq != parsed_pdb_row['resSeq']:
+					if self.verbose > 2:
+						print(f"    Starting Residue: {parsed_pdb_row['resSeq']}")
 					current_resSeq = parsed_pdb_row['resSeq']
 					residue.add_atoms(current_residue_atom_list)
 					current_residue_atom_list = []
@@ -70,7 +80,9 @@ class PDBParser:
 										float(parsed_pdb_row['y']),
 										float(parsed_pdb_row['z'])
 									 ])
-				
+				if self.verbose > 3:
+					print(f"      Starting Atom: {parsed_pdb_row['name']} ({parsed_pdb_row['altLoc']}) \
+								with element {parsed_pdb_row['element']} at coordinate {parsed_pdb_row}")
 				atom = Atom(parsed_pdb_row['name'],parsed_pdb_row['element'], coordinate,
 									parsed_pdb_row['altLoc'] == 'CA')
 				current_residue_atom_list.append(atom)
@@ -88,12 +100,14 @@ class PDBParser:
 			path = Path(path_to_pdb)
 			complex_file_name = path.stem
 			pickle_path = './processedComplex/'+complex_file_name+'.pkl'
+		if self.verbose:
+			print(f'Parsing {path_to_pdb} to {pickle_path}')
 		complex_structure = self.parse_pdb(path_to_pdb)
 		path = Path(pickle_path)
 		with path.open() as f:
 			pickle.dump(complex_structure, f)
 
-def parse_pdb_from_pickle(pickle_path):
+def parse_pdb_from_pickle(pickle_path, verbose=False):
 	'''
 	Gets Complex structure from pickle file
 
@@ -101,6 +115,8 @@ def parse_pdb_from_pickle(pickle_path):
 		pickle_path : string :: Path to pickle file
 	'''
 	path = Path(pickle_path)
+	if verbose:
+		print(f"Unpickling {pickle_path} into Complex")
 	with path.open() as f:
 		complex_structure = pickle.dump(f)
 	return complex_structure
@@ -152,7 +168,7 @@ if __name__ == "__main__":
                        metavar='path',
                        type=str,
                        help='the path to file')
-	parser.add_argument('--verbose', '-v', action='store_true',
+	parser.add_argument('--verbose', '-v', action='count',
 						help='set to have debug outputs')
 	parser.add_argument('--local', '-l', action='store_true',
 						help='set to calculate local system')
